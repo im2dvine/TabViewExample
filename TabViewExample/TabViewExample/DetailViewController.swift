056d15ae17797b78dd9c9e93cfc97559d133f5a3
Daesy <daesy@edfarm.org>
2022-11-15 12:47:32 -0600

    completed first part of assignment

--- TabViewExample/TabViewExample/DetailViewController.swift
+++ TabViewExample/TabViewExample/DetailViewController.swift
@@ -1,10 +1,3 @@
-//
-//  DetailViewController.swift
-//  TabViewExample
-//
-//  Created by Daesy Vences on 11/15/22.
-//
-
 import UIKit
 
 class DetailViewController: UIViewController {
@@ -15,15 +8,5 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
